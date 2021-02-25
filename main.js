7ba667276010c64abafe22027651795fcb2c12a6
Dong Song Hyun <dst406@naver.com>
2021-02-26 00:21:09 +0900

    JS: Navbar menuclick

---
+++ main.js
@@ -0,0 +1,26 @@
+"use strict";
+
+//Make navbar transparent
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
+
+// Handle scrolling
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", event => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  } else {
+    console.log(event.target.dataset.link);
+  }
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+});
