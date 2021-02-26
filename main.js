e49b7769c3cf8d9a7c7f5f17b4b447a2d81e3d71
Dong Song Hyun <dst406@naver.com>
2021-02-26 18:45:17 +0900

    CSS: Final

--- main.js
+++ main.js
@@ -18,9 +18,46 @@ navbarMenu.addEventListener("click", event => {
   const link = target.dataset.link;
   if (link == null) {
     return;
-  } else {
-    console.log(event.target.dataset.link);
   }
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
+  navbarMenu.classList.remove("open");
+  scrollIntoView(link);
 });
+
+//Navbar toggle button
+const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
+navbarToggleBtn.addEventListener("click", () => {
+  navbarMenu.classList.toggle("open");
+});
+
+// Handle "contact me"
+const homeContactBtn = document.querySelector(".home__contact");
+homeContactBtn.addEventListener("click", () => {
+  scrollIntoView("#contact");
+});
+
+//Fade to transparent
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+//Show "arrow up"
+const arrowUp = document.querySelector(".arrow-up");
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add("visible");
+  } else {
+    arrowUp.classList.remove("visible");
+  }
+});
+
+//Handle click arrow up
+arrowUp.addEventListener("click", () => {
+  scrollIntoView("#home");
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
